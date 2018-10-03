1a6770150930e51dce76ebd09be0f392e26e8ebd
krhubert <krhubert@gmail.com>
2018-10-03 11:52:23 +0200

    Show go-bindata example

---
+++ docker-images/go-bindata/go.mod
@@ -0,0 +1,3 @@
+module main
+
+require github.com/go-bindata/go-bindata v1.0.0
---
+++ docker-images/go-bindata/main.go
@@ -0,0 +1,6 @@
+package main
+
+// import go-bindata for allow vendoring (`go mod vendor`)
+// otherwise go mod vendor returns
+// go: no dependencies to vendor
+import _ "github.com/go-bindata/go-bindata/go-bindata"
